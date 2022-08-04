9f35d41fb152cc37c14e7b1637be98c3fcbdbd54
nacn <canchila.sanchez@gmail.com>
2022-08-04 16:10:43 -0500

    bien

---
+++ wssKNCk5.py
@@ -0,0 +1,36 @@
+import asyncio, os
+from binance import AsyncClient, BinanceSocketManager 
+from binance.enums import *
+
+async def main():
+	client = await AsyncClient.create()
+	bm = BinanceSocketManager(client)
+	sts = bm.kline_socket("KNCUSDT", interval = KLINE_INTERVAL_1HOUR)
+
+
+	async with sts as sm :
+		while True:
+			res = await sm.recv()
+			os.system("clear")
+			cl = float(res['k']['c'])
+			op = float(res['k']['o'])
+			hi = float(res['k']['h'])
+			lo = float(res['k']['l'])
+			pct = cl/100
+
+
+			print(
+				f"KNC H1\nPorcentaje Open/Close:\n{float(cl-op)/pct}\nPorcentaje High/Low:\n{float(hi-lo)/pct}"
+				)
+
+
+if __name__ == "__main__":
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(main())
+
+
+
+
+
+
+
